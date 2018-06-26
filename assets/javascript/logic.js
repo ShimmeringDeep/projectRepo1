f848034406fd15598ad0651a72b9c6142e65498a
Jordan Vera <verawebdevelopment@gmail.com>
2018-06-26 12:38:39 -0500

    Major updates with data output on the front-end side (changed the way the data was being displayed for responsiveness) as well as stylesheet changes.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -12,13 +12,13 @@ firebase.initializeApp(config);
 var database = firebase.database() // firebase variables
 
 
-const colors = ["orange", "green", "blue", "red", "purple", "white", "yellow"]
+const colors = ["Orange", "Green", "Blue", "Red", "Purple", "White", "Yellow"]
 
 const locKey = "AIzaSyAS7eVhjgd7vDmyAMGzCzygJ2q1sf1kXKo" //geolocation API key for later implementation (ajax call working at bottom)
 const eventkey = "WVV7pQ6XZXdVrR9r" //eventful API key
 
 
-$("#submit").on("click", function (event) { //submit button click event (ajax call for eventful and DOM population happens in here)
+$("#submit").on("click", function (e) { //submit button click event (ajax call for eventful and DOM population happens in here)
 
     let eventUrl = "http://api.eventful.com/json/events/search" //query url for eventful
     var locationInput = $("#eventLocationQuery").val().trim(); //grabs text value from the text imput field
@@ -26,7 +26,10 @@ $("#submit").on("click", function (event) { //submit button click event (ajax ca
 
     eventUrl += '?' + $.param({ //parameters for eventful query (we need a few more of these to make the returns cleaner)
         'app_key': eventkey,
-        'location': locationInput
+        'location': locationInput,
+        'date': 'thisweek',
+        'category': 'music , comedy, festivals_parades, food, art, holiday, singles_social, outdoors_recreation, sports',
+        'sort_order': 'popularity'
 
     })
 
@@ -36,41 +39,158 @@ $("#submit").on("click", function (event) { //submit button click event (ajax ca
         dataType: 'jsonp'
     }).then(function (result) {//after ajax query to eventful
         var events = result.events.event; //grabs the location in the result where the events from our query are stored
+        console.log(events);
+        database.ref().once('value', function (snapshot) {
 
-        for (i = 0; i < events.length; i++) { //for loop populates the DOM and firebase with our returned events
+            for (var i = 0; i < events.length; i++) { //for loop populates the DOM and firebase with our returned events
+                var event = events[i]; //grabs the event in the event array at location [i]
+                var eventObj = {
+                    title: event.title,
+                    venue: event.venue_name,
+                    start: event.start_time,
+                    address: event.venue_address,
+                    description: event.description,
+                    eventID: event.id,
+                    color: colors[Math.floor(Math.random() * colors.length)],
+                    attendees: 0,
+                    lat: event.latitude,
+                    long: event.longitude
+                };
 
-            //TO DO!!! MAKE AN IF THEN STATEMENT THAT ONLY CREATES NEW FIREBASE ENTRIES FOR NON EXISTING EVENTS (so we don't overwrite color or attendance)
+                var checkArr = [];
+                snapshot.forEach(function (childSnapshot) {
+                    if (childSnapshot.val().eventID === event.id) {
+                        checkArr.push(childSnapshot.val());
+                    }
+                });
 
-            var event = events[i]; //grabs the event in the event array at location [i]
+                if (checkArr.length > 0) {
+                    console.log(checkArr[0]);
+                    eventObj = checkArr[0];
+					  
+					  let results = `
+					  <div data-event-\id\="${event.id}" class="results">
+					  	<div>
+					  		<p><span class="bold">title:</span> ${eventObj.title}</p>
+					  		
+					  		<p><span class="bold">time:</span> ${eventObj.start}</p>
+					  		
+					  		<p><span class="bold">venue:</span> ${eventObj.venue}</p>
+					  	</div>
+					  	
+					  	<div class="rightModalSection">
+					  		<p><span class="bold">attendees:</span> ${eventObj.attendees}</p>
+					  		
+					  		<p><span class="bold">wristband color:</span> ${eventObj.color}</p>
+					  		
+					  		<button class="button small openModal" data-open="${event.id}">More Info!</button>
+					  	</div>
+					  </div>
+					  <hr />
+					  `;
+	
+					  document.querySelector('#primaryResults').innerHTML += results;
+                } else {
+                   let results = `
+                   	<div data-event-\id\="${event.id}" class="results">
+                   		<div>
+                   			<p><span class="bold">title:</span> ${eventObj.title}</p>
+                   				  		
+                   			<p><span class="bold">time:</span> ${eventObj.start}</p>
+                   				  		
+                   			<p><span class="bold">venue:</span> ${eventObj.venue}</p>
+                   		</div>
+                   				  	
+                   		<div class="rightModalSection">
+                   			<p><span class="bold">attendees:</span> ${eventObj.attendees}</p>
+                   				  		
+                   			<p><span class="bold">wristband color:</span> ${eventObj.color}</p>
+                   				  		
+                   			<button class="button small openModal" data-open="${event.id}">More Info!</button>
+                   		</div>
+                   	</div>
+                   	<hr /> `;
+                   
+                   	document.querySelector('#primaryResults').innerHTML += results;
 
-            //create an event obj for easier storage and manipulation (good practice)
-            var eventObj = {
-                title: event.title,
-                venue: event.venue_name,
-                start: event.start_time,
-                address: event.venue_address,
-                description: event.description,
-                eventID: event.id,  //build items on dom
-                color: colors[Math.floor(Math.random() * colors.length)],
-                attendees: 0
-            };
-
-            var newRow = $("<tr>").attr("class", "eventRow").attr("data-event-id", event.eventID); //creates a new row with a class for styling and scripting and a data with the unique ID of the event stored within it
-            titleD = $("<td>").text(eventObj.title); //create table data elements with text content
-            dateD = $("<td>").text(eventObj.start);
-            colorD = $("<td>").text(eventObj.color); //TO DO get this variable from firebase instead
-            venueD = $("<td>").text(eventObj.venue);
-            attendeesD = $("<td>").text(eventObj.attendees); //TO DO get this variable from firebase instead
-            newRow.append(titleD, dateD, colorD, venueD, attendeesD); // appends that text content to the newRow we declared earlier
-            $("#results").append(newRow) //appends that newRow (along with its children) to the DOM
-
-
-            database.ref().push(eventObj);
-            // store event in firebase
-
-        }
-    })
+                    database.ref().push(eventObj)                  }
+            }
+        });
+    });
 });
+ 
+
+
+$("body").on("click", ".openModal", function (event) {
+	let modalObj;
+	let buttonId = $(this).attr('data-open');
+	database.ref().once('value').then(function(snapshot) {
+		
+		snapshot.forEach(function (childSnapshot) {
+			if (childSnapshot.val().eventID === buttonId) {
+				modalObj = childSnapshot.val();
+			}
+		});
+	
+	
+    let modals = `
+		<div class="reveal" \id\="${modalObj.eventID}" data-reveal data-animation-\in\="fade-in" data-animation-\out\="fade-out">
+			<div class="results">
+				<div>
+					<p><span class="bold">title:</span> ${modalObj.title}</p>
+					
+					<p><span class="bold">address:</span> ${modalObj.address}</p>
+					
+					<p><span class="bold">venue:</span> ${modalObj.venue}</p>
+				</div>
+				
+				<div class="rightModalSection">
+					<p><span class="bold">time:</span> ${modalObj.start}</p>
+					<p><span class="bold">wristband color:</span> ${modalObj.color}</p>
+			  		<a href="${modalObj.url}" class="button tiny" target="_blank">Check Out Their Website!</a>
+			  	</div>
+				
+				<div \id\="googleMap">
+					<div \id\="map"></div>
+				</div>
+				
+				<div \id\="attendee">
+					<p><span class="bold">attendees:</span> ${modalObj.attendees}</p>
+				</div>
+				
+				<div \id\="rsvp" class="rightModalSection">
+					<button class="button tiny">RSVP now!</button>
+				</div>
+			</div>
+			
+			<button class="close-button" data-close aria-label="Close modal" type="button"><span aria-hidden="true">&times;</span></button>
+		</div>
+	`;
+	
+	document.body.innerHTML += modals;
+	$("#"+ modalObj.eventID).foundation();
+	$("#"+ modalObj.eventID).foundation('open');
+	
+	initMap (modalObj.lat, modalObject.long)
+	
+    });
+	
+	
+}); // end of the .openModal click event
+
+
+
+function initMap(latitude, longitude) {
+  var markerLocation = {lat: latitude, lng: longitude};
+  var map = new google.maps.Map(
+  document.getElementById('map'), {zoom: 12, center: markerLocation});
+  
+  var marker = new google.maps.Marker({position: markerLocation, map: map});
+}
+
+
+
+
 
 
 let locateUrl = "https://www.googleapis.com/geolocation/v1/geolocate?key=" + locKey //geolocation url
@@ -79,33 +199,5 @@ $.ajax({ //geolocation ajax request
     url: locateUrl,
     method: 'POST',
 }).then(function (result) {
-    console.log(result); //returns latitude and longitude and accuracy
+//    console.log(result); //returns latitude and longitude and accuracy
 })
-
-    /* just logic i'm saving for later, related to pulling data out of firebase
-    
-    click function
-    snapshot.forEach()
-    ref.on("child_added", function(child) {
-        console.log(child.key+': '+child.val());
-        var tr = document.createElement('tr');
-        var td = document.createElement('td');
-        td.innerText = child.val().email + " --- " + JSON.stringify(child.val());
-        tr.appendChild(td);
-        table.appendChild(tr);
-    });
-    
-    
-    var eventID = $("somefuckingelementonthepage").attr("data-event-id")
-    var newFilteredArray array.filter(function(element){
-        return id === eventID;
-    });
-    
-    
-    
-    getTheChildSnapshots(function(child){
-        if(child.id === eventID){
-            do something
-        }
-    })
-    */
